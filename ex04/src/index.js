b90e300771f9c0644d1cd2ba9a934b61e872c88e
oarvin <oprasicarvin@gmail.com>
2021-09-16 11:15:48 +0200

    Decimal Numbers

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+  
+    // Comment
+    var myDecimal = 8.2;
+    var myDecimalE = 254e-5
+    // Comment
+    return{
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
